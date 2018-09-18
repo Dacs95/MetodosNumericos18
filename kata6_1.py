0de8d6c87c3775cd052b90349424981d5492681b
Daniel <a01017852@itesm.mx>
2018-09-18 09:25:17 -0500

    Update kata6_1.py

--- kata6_1.py
+++ kata6_1.py
@@ -1,3 +1,13 @@
+'''
+Return the Factorial
+Create a function that takes an integer and returns the factorial of that integer. That is, the integer multiplied by all positive lower integers.
+Examples
+3 ➞ 6
+
+5 ➞ 120
+
+13 ➞ 6227020800
+'''
 def factorial(num):
 	if num == 0:
 		return 1
